ad9ff8abec50edbb9be719e30063eb931a28ad17
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-06 22:40:45 +0600

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Provider } from "react-redux";
+import { store } from "@/app/redux/store";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
